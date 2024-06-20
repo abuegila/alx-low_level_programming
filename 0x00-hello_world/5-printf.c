a1f05b1bdb3cca93dbde232d6e9db59e49a532e0
abuegila <yusufabuegila@gmail.com>
2024-06-20 07:33:55 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio>
+/**
+ * main - a program that prints a line using printf
+ * Return: 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+}
