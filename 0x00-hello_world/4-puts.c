8aa76c4bcd638f7188a3ae4645586a696cb2ee91
abuegila <yusufabuegila@gmail.com>
2024-06-20 07:24:51 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - this program is printing a line using puts
+ *then it's ending by zero
+ *Return: 0
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
