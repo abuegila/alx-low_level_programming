26f8a2869efe5a0b72887bbb14f4484c7e02a0a6
abuegila <yusufabuegila@gmail.com>
2024-06-20 08:40:00 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+/**
+ * main-a programg that prints using linux system
+ * Return:0
+ */
+int main(void)
+{
+system("echo 'and that piece of art is useful\" - Dora Korpar, 2015-10-19' >&2");
+return (0);
+}
+
