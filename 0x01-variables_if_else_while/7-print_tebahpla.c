54c92937c39140ca4dae218df2b1d33945e5a434
abuegila <yusufabuegila@gmail.com>
2024-06-21 04:43:05 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main-Prints all single digit numbers of base 10 starting from 0.
+ * Return: Always 0.
+ */
+int main(void)
+{
+int i;
+for (i = 122; i >= 97; i--)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
