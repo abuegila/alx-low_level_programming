62d069fa5c42ea23cc8c0161364bebbe233bf939
abuegila <yusufabuegila@gmail.com>
2024-06-21 04:23:40 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main-prints the alphabet.
+ * Return: Always 0.
+ */
+int main(void)
+{
+for (int i = 97 ; i < 123 ; i++)
+{
+putchar(i);
+}
+return (0);
+}
