995b60ab19aba3e30c7108a4538cd6cdc09f689b
abuegila <yusufabuegila@gmail.com>
2024-06-21 04:17:14 -0700

    updating 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,10 +14,10 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 tmp = n % 10;
 if (tmp < 6 && tmp != 0)
-printf("Last digit of %d is %d and is less than 6 and not 0", n, tmp);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, tmp);
 else if (tmp > 5)
-printf("Last digit of %d is %d and is greater than 5", n, tmp);
+printf("Last digit of %d is %d and is greater than 5\n", n, tmp);
 else
-printf("Last digit of %d is %d and is 0", n, tmp);
+printf("Last digit of %d is %d and is 0\n", n, tmp);
 return (0);
 }
