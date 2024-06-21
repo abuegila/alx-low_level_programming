f71bc5e0ca4e64d9362ac36e94529761b20dfeba
abuegila <yusufabuegila@gmail.com>
2024-06-21 05:00:14 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main-prints all possible different combinations of three digits.
+ * Return: Always 0.
+ */
+int main(void)
+{
+int hundreds, tens, ones;
+for (hundreds = 0; hundreds <= 7; hundreds++)
+{
+for (tens = hundreds + 1; tens <= 8; tens++)
+{
+for (ones = tens + 1; ones <= 9; ones++)
+{
+putchar(hundreds + '0');
+putchar(tens + '0');
+putchar(ones + '0');
+if (hundreds != 7 || tens != 8 || ones != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
