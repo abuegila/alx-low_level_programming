d2e015f6e09b690f279655f0f609ec52d0c0173c
abuegila <yusufabuegila@gmail.com>
2024-06-21 04:14:25 -0700

    updating 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,7 +12,7 @@ int tmp;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-tmp = n & 10;
+tmp = n % 10;
 if (tmp < 6 && tmp != 0)
 printf("Last digit of %d is %d and is less than 6 and not 0", n, tmp);
 else if (tmp > 5)
