e8521a3824f398666b7b0d645210b1ea0ffc559e
abuegila <yusufabuegila@gmail.com>
2024-06-25 13:34:54 -0700

    final 102-fibonacci. el 7amod le llah

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -12,11 +12,14 @@ long tmp;
 
 num = 0;
 value = 1;
-for (i = 1 ; i < 50 ; i++)
+for (i = 1 ; i <= 50 ; i++)
 {
 tmp = value;
 value += num;
 num = tmp;
+if (i == 50)
+printf("%lu", value);
+else
 printf("%lu, ", value);
 }
 return (0);
