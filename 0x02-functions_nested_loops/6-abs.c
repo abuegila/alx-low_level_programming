86781be0c05f33dbd7ed00e4e87ce4347a96ca7d
abuegila <yusufabuegila@gmail.com>
2024-06-25 08:30:30 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - returns the absluote of a number
+ * @n: a paramter to be checked
+ * Return: the absluote of a number
+ */
+int _abs(int n)
+{
+if (n < 0)
+return (-n);
+else
+return (n);
+}
