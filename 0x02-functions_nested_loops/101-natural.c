dfe61c46e785e66382ef5d73aea177eaa50fe379
abuegila <yusufabuegila@gmail.com>
2024-06-25 12:22:52 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - printing the multiples of 3 and 5 till 1024
+ * Return: Always 0
+ */
+int main(void)
+{
+int i;
+int sum;
+
+sum = 0;
+for (i = 0 ; i < 1024 ; i++)
+{
+if (i % 3 == 0 || i % 5 == 0)
+sum += i;
+}
+printf("%d", sum);
+return (0);
+}
