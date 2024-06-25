c707fc3e755f55231fa070eeed96f3c0c48784e8
abuegila <yusufabuegila@gmail.com>
2024-06-25 07:45:24 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main"
+/**
+ * print_alphabet_x10 - printing the alphabet 10 times
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+int i;
+int j;
+for (j = 0 ; j < 10 ; j++)
+{
+for (i = 97 ; i < 123 ; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
+}
