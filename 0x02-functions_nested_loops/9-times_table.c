cd96440404343162ef7a3835b59b43c72447287d
abuegila <yusufabuegila@gmail.com>
2024-06-25 10:07:11 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * times_table - printing time table to 9
+ * Return: void
+ */
+void times_table(void)
+{
+int i;
+int j;
+int value;
+int last_digit;
+
+for (i = 0 ; i < 10 ; i++)
+{
+for (j = 0 ; j < 10 ; j++)
+{
+value = i * j;
+if (value > 9)
+{
+last_digit = value % 10;
+value /= 10;
+_putchar(value + '0');
+_putchar(last_digit + '0');
+_putchar(',');
+_putchar(' ');
+continue;
+}
+_putchar(value + '0');
+_putchar(',');
+_putchar(' ');
+}
+_putchar('\n');
+}
+}
