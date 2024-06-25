4829d835363c491d77117e15e01d5918eead4164
abuegila <yusufabuegila@gmail.com>
2024-06-25 08:54:17 -0700

    7-print_last_digit.c last edit isa

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,8 +9,9 @@ int print_last_digit(int n)
 int last_digit;
 
 if (n < 0)
-n = (-n);
+last_digit = (n % 10) * -1;
+else
 last_digit = n % 10;
-_putchar(last_digit + 48);
+_putchar(last_digit + '0');
 return (last_digit);
 }
