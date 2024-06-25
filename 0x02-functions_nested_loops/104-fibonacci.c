5b44b1be27b11247dba01c364b0f2a58ccdb308d
abuegila <yusufabuegila@gmail.com>
2024-06-25 13:59:50 -0700

    104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - printing the first 98 numbers of fabionaci series
+ * Return: Always 0
+ */
+int main(void)
+{
+unsigned long a = 1, b = 2, temp;
+printf("%lu,%lu", a, b);
+for (int i = 3 ; i <= 98 ; i++)
+{
+temp = a + b;
+a = b;
+b = temp;
+printf(",%lu", b);
+}
+printf("\n");
+return (0);
+}
