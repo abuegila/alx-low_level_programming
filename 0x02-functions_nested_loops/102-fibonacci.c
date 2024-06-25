498577bf340750b1d6460a9b6d37aecfd010b70e
abuegila <yusufabuegila@gmail.com>
2024-06-25 13:06:38 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * main - printing the first 50 number of fabionaci series
+ * Return: Always 0
+ */
+int main(void)
+{
+int i;
+int num;
+int value;
+int tmp;
+
+num = 0;
+value = 1;
+for (i = 1 ; i < 50 ; i++)
+{
+tmp = value;
+value += num;
+num = tmp;
+printf("%d, ", value);
+}
+return (0);
+}
