1f5742e5a9c9261fd39bb91c806b85546a7ef9dc
abuegila <yusufabuegila@gmail.com>
2024-06-27 07:48:00 -0700

    8-print_square.c git push

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_square - printing a square shape
+ * @size: the size of the square shape
+ * Return: void
+ */
+
+void print_square(int size)
+{
+int i;
+int j;
+
+for (i = 0 ; i < size ; i++)
+{
+for (j = 0 ; j < size ; j++)
+{
+_putchar('#');
+}
+}
+}
