ce7e0f7f0f619ffb5cf368712ca5e39aa31e4057
abuegila <yusufabuegila@gmail.com>
2024-07-03 15:46:33 -0700

    4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * reverse_array - reversing an array
+ * @a: the array to be reversed
+ * @n: the size of the array
+ * Return: void
+ */
+void reverse_array(int *a, int n)
+{
+int temp;
+
+for (int i = 0 ; i < n / 2 ; i++)
+{
+temp = a[i];
+a[i] = a[n - 1 - i];
+a[n - 1 - i] = temp;
+}
+}
