2804f955698b7844d9e9391055f09c9e652ccfd3
Yusuf Abu Egila <56008550+abuegila@users.noreply.github.com>
2024-07-04 03:14:50 +0300

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,24 +7,19 @@
 char *leet(char *n)
 {
 char *ptr = n;
-char leet_map[256] = {0};
-
-leet_map['a'] = '4';
-leet_map['e'] = '3';
-leet_map['o'] = '0';
-leet_map['t'] = '7';
-leet_map['l'] = '1';
-leet_map['A'] = '4';
-leet_map['E'] = '3';
-leet_map['O'] = '0';
-leet_map['T'] = '7';
-leet_map['L'] = '1';
+    
 while (*ptr != '\0')
 {
-if (leet_map[*ptr] != 0)
-{
-*ptr = leet_map[*ptr];
-}
+if (*ptr == 'a' || *ptr == 'A')
+*ptr = '4';
+else if (*ptr == 'e' || *ptr == 'E')
+*ptr = '3';
+else if (*ptr == 'o' || *ptr == 'O')
+*ptr = '0';
+else if (*ptr == 't' || *ptr == 'T')
+*ptr = '7';
+else if (*ptr == 'l' || *ptr == 'L')
+*ptr = '1';
 ptr++;
 }
 return (n);
