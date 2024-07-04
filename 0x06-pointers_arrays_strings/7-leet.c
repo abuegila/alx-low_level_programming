340dd199689830c7e51954fb05cfa540b615ee2e
Yusuf Abu Egila <56008550+abuegila@users.noreply.github.com>
2024-07-04 03:04:39 +0300

    Update 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -4,25 +4,27 @@
  * @n: the string to be processed
  * Return: a pointer to the string
  */
+#include "main.h"
+
 char *leet(char *n)
 {
-int i;
-char *current_char = n;
-char lower_case_letters[] = {'a', 'e', 'o', 't', 'l'};
-char upper_case_letters[] = {'A', 'E', 'O', 'T', 'L'};
-char leet_numbers[] = {'4', '3', '0', '7', '1'};
-    
-while (*current_char != '\0')
-{
-for (i = 0; i < 5; i++)
-{
-if (*current_char == lower_case_letters[i] || *current_char == upper_case_letters[i])
-{
-*current_char = leet_numbers[i];
-break;
-}
-}
-current_char++;
-}
-return (n);
+    char *current_char = n;
+    char original_letters[] = {'a', 'e', 'o', 't', 'l', 'A', 'E', 'O', 'T', 'L'};
+    char leet_equivalents[] = {'4', '3', '0', '7', '1', '4', '3', '0', '7', '1'};
+    int i;
+
+    while (*current_char != '\0')
+    {
+        for (i = 0; i < 10; i++)
+        {
+            if (*current_char == original_letters[i])
+            {
+                *current_char = leet_equivalents[i];
+                break;
+            }
+        }
+        current_char++;
+    }
+
+    return (n);
 }
