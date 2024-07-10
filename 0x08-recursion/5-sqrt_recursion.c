bb193d91e630fd7f84c1230ce088a60b87a4c53f
abuegila <yusufabuegila@gmail.com>
2024-07-10 16:45:10 +0300

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+_sqrt_recursion(int n);
+/**
+ * check_n - getting the number
+ * @n: the number to be checker
+ * @initial_value: always 0
+ * Return: -1 if not found or value if found
+ */
+int check_n(int n, int initial_value)
+{
+if (initial_value > n)
+return (-1);
+if (initial_value * initial_value == n)
+return (initial_value);
+return (check_n(n, initial_value + 1));
+}
+/**
+ * _sqrt_recursion - getting the sqrt of a num
+ * @n: the number to be checked
+ * Return: -1 if not found or value if found
+ */
+int _sqrt_recursion(int n)
+{
+if (n < 0)
+return (-1);
+return (check_n(n, 0));
+}
