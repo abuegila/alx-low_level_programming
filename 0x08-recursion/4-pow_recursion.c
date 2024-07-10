ec5afe65af2129e854d2fb7831c2e71a13f34a5d
abuegila <yusufabuegila@gmail.com>
2024-07-10 16:20:41 +0300

    4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _pow_recursion - clac pow by rec
+ * @x: the number to be calculated
+ * @y: the power
+ * Return: the value of multplying
+ */
+int _pow_recursion(int x, int y)
+{
+if (x < 0)
+return (-1);
+if (y < 1)
+return (1);
+return (x * _pow_recursion(x, y - 1));
+}
