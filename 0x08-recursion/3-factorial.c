d564d8a76c401714bc2c5525348330ca696394f2
abuegila <yusufabuegila@gmail.com>
2024-07-10 16:14:33 +0300

    3-factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * factorial - calculating the fac of a num
+ * @n: the number to be processed
+ * Return: the result of fac
+ */
+int factorial(int n)
+{
+if (n == 0)
+return (1);
+return (n * factorial(n - 1));
+}
