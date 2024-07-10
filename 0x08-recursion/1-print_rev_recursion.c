0a9485a6cf3f799858f98251182aef7dbc94a635
abuegila <yusufabuegila@gmail.com>
2024-07-10 16:00:45 +0300

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - printing rev string
+ * @s: the string to be printed
+ * Return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+if (*s)
+{
+_puts_recursion(s + 1);
+_putchar(*s);
+}
+}
