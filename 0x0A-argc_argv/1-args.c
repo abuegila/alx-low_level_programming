d9e6a2830dd51c8989b8eace304f77df24fb62bf
abuegila <yusufabuegila@gmail.com>
2024-07-12 13:25:32 +0300

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - printing the number arguments
+ * @argc: the number of arguments
+ * @argv: the array of arguments
+ * Return: Always 0
+ */
+int main(int argc, char const *argv[])
+{
+printf("%d", argc)
+return (0);
+}
