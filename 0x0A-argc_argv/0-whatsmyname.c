a1dd31266a4f2fce76c4412cd02d74503c42f062
abuegila <yusufabuegila@gmail.com>
2024-07-12 13:05:31 +0300

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - printing the name of the prog
+ * @argc: the num or arguments
+ * @argv: the array of arguments
+ * Return: Always 0
+ */
+int main(int argc, char const *argv[])
+{
+(void)argc;
+printf("%s\n", argv[0]);
+return(0);
+}
