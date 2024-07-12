330cc075a384d717c47210e4b5dc4bf055d67dee
abuegila <yusufabuegila@gmail.com>
2024-07-12 13:28:37 +0300

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,7 +5,7 @@
  * @argv: the array of arguments
  * Return: Always 0
  */
-int main(int argc, char const *argv[])
+int main(int argc, char __attribute__((__unused__))  *argv[])
 {
 printf("%d\n", argc)
 return (0);
