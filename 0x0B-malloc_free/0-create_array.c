54ef8ef1b0756b14d6aadfdfff588969066546f7
abuegila <yusufabuegila@gmail.com>
2024-07-16 17:11:15 +0300

    0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * create_array - creating a dynamic array
+ * @size: the size of the array
+ * @c: the char to fill the array with
+ * Return: a pointer to the array
+ */
+char *create_array(unsigned int size, char c)
+{
+char *cc;
+int i;
+
+if (size == 0)
+return (NULL);
+cc = (char)malloc(sizeof(char) * size);
+if (cc == NULL)
+return (NULL);
+for (i = 0; i < size; i++)
+cc[i] = c;
+return (cc);
+}
