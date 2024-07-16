8ccb8d4d4ca6aff699361f38bce1a1b6e08ca045
abuegila <yusufabuegila@gmail.com>
2024-07-16 17:24:10 +0300

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* _strdup - copying an array
+*@str:String to be copied
+*Return: a pointer to the string, Null if not
+*/
+
+char *_strdup(char *str)
+{
+char *cp;
+int i, length;
+
+if (str == NULL)
+return (NULL);
+for (i = 0; str[i]; i++)
+length++;
+cp = malloc(sizeof(char) * (length + 1));
+if (cpy == NULL)
+return (NULL);
+for (i = 0; str[i]; ++)
+{
+cp[i] = str[i];
+}
+cp[length] = '\0';
+return (cp);
+}
