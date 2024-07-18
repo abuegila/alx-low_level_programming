2d26f1eca6e207dd9ee599142384dfb45a7530de
abuegila <yusufabuegila@gmail.com>
2024-07-18 14:42:50 +0300

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,6 +1,5 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
+#include <stdio.h>
 #include "main.h"
 /**
  * malloc_checked - allocating memory in heap
@@ -13,6 +12,9 @@ void *ptr;
 
 ptr = malloc(b);
 if (ptr == NULL)
+{
+free(ptr)
 exit(98);
+}
 return (ptr);
 }
