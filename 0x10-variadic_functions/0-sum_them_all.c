7b3b8621ca494059078bcf3ff42b916815c49447
abuegila <yusufabuegila@gmail.com>
2024-07-26 01:07:10 +0300

    0-sum_them_all.c

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,20 @@
+#include <stdarg.h>
+/**
+ * sum_them_all - getting the sum of pars
+ * @n: the number of args
+ * Return: the resulting sum, 0 if not
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+int x = 0;
+int sum = 0;
+va_list ss;
+va_start(ss, n);
+
+if (n == 0)
+return (0);
+for (int i = 0; i < n; i++)
+sum += va_arg(ss, int);
+va_end(ss);
+return (sum);
+}
